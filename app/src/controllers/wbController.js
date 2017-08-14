01cb77e0c0775f37cea4a494d8ec7f6be81d26fb
Methusael Murmu <blendit07@gmail.com>
2017-08-15 03:57:29 +0530

    Fixes app login and signup

--- app/src/controllers/wbController.js
+++ app/src/controllers/wbController.js
@@ -50,11 +50,9 @@ module.exports = function(app) {
 	});
 
 	app.get('/', function (req, res) {
-		
 		res.render('base');
 	});
 	app.get('/signup', function (req, res) {
-		
 		res.render('signup');
 	});
 
--- app/src/public/assets/js/auth.js
+++ app/src/public/assets/js/auth.js
@@ -1,72 +1,133 @@
-$('#signup_btn').on('click', function () {
-    var $name = $('#name');
-    var $usern = $('#username');
-    var $pass = $('#password');
-    var $email = $('#email');
+
+var def_log = (e, err = true) => {
+    var dt = (new Date()).toLocaleString();
+    console.log("[" + (err ? "ERROR" : "INFO ") + " | " + dt + "]: " + e);
+}
+
+const def_headers = {
+    "Content-Type": "application/json;charset=utf-8"
+}
+
+var ajaxp = (_url, _data, _headers, success, error = def_log) => {
+    if (!window.XMLHttpRequest) {
+        def_log("Cancelled ajax call. Perhaps update the browser?");
+        return;
+    }
     
-    // Populate user data
-    app.setUserName($usern.val());
-    app.setUserInfo({
-        name: $name.val(),
-                    email: $email.val(),
-    });
-    
-    var userd = {
-        username: app.user.username,
-        password: $pass.val(),
-                    email: app.user.email
+    var xhr = new XMLHttpRequest();
+    let callback = () => {
+        if (xhr.readyState == XMLHttpRequest.DONE) {
+            if (xhr.status == 200) {
+                success(xhr.responseText);
+            } else {
+                def_log("Server responded with code: " + xhr.status);
+                error(xhr.responseText);
+            }
+        }
     };
     
-    $.ajax({
-        method: 'POST',
-        url: app.urls.auth + "signup",
-        
-        headers: { 'Content-Type': 'application/json' },
-        data: JSON.stringify(userd)
-    }).done(function(user) {
-        console.log("Received data: " + user);
-        console.log("Successfully signed up. Redirecting to app...");
-        
-        // Save critical data
-        var user_obj = JSON.parse(user);
-        app.setUserInfo({
-            hasura_id: user_obj.hasura_id,
-            hasura_roles: user_obj.hasura_roles,
-            auth_token: user_obj.auth_token
-        });
-        
-        // Save additional user data to server
-        var prof_data = {
-            "type": 'insert',
-            "args": {
-                "table": 'profile',
-            "objects": [{
-                "id": app.user.id,
-            "name": app.user.name
-            }]
-            }
-        };
-        
-        $.ajax({
-            method: 'POST',
-            url: app.urls.data + 'v1/query',
+    xhr.onreadystatechange = callback.bind(xhr);
+    xhr.withCredentials = true;
+    
+    xhr.open("POST", _url, true);
+    for (var key in _headers) {
+        if (_headers.hasOwnProperty(key))
+            xhr.setRequestHeader(key, _headers[key]);
+    }
+    xhr.send(_data);
+} 
+
+var signup = (_name, _usern, _email, _password) => {
+    // Populate user data
+    app.setUserName(_usern);
+    app.setUserInfo({ name: _name, email: _email });
+    
+    let userd = {
+        "username": app.user.username,
+        "password": _password,
+        "email": app.user.email
+    };
+    
+    ajaxp(
+        app.urls.auth + "signup", JSON.stringify(userd), def_headers,
+        (sdata) => {
+            def_log("Received data: " + sdata, false);
             
-            headers: { 'Content-Type': 'application/json' },
-            data: JSON.stringify(prof_data)
-        }).done(function(data) {
-            console.log(data);
+            let user = JSON.parse(sdata);
+            app.setUserInfo({
+                hasura_id: user["hasura_id"],
+                hasura_roles: user["hasura_roles"],
+                auth_token: user["auth_token"]
+            });
+
+            // Save profile data to server
+            let prof_data = {
+                "type": "insert",
+                "args": {
+                    "table": "profile",
+                    "objects": [{
+                        "id": app.user.id,
+                        "name": app.user.name
+                    }]
+                }
+            };
             
-            // Redirect to application
-            window.location = "/app";
-        }).fail((e) => { console.log(e); });
-    }).fail((e) => {
-        // Handle errors
-        // Premise: No change of page (dynamically generated)
-        if (e) {
-            console.log(e);
-            console.error("Signup error! Reason: " + e.responseText);
-        } else {
-            console.error("Fuck e");
+            ajaxp(
+                app.urls.data + "v1/query", JSON.stringify(prof_data), def_headers,
+                (sresp) => {
+                    let resobj = JSON.parse(sresp);
+                    def_log(resobj["message"], false);
+                    
+                    def_log("Signup complete. Redirecting to app...", false);
+                    window.location.href = "/app";
+                }
+            );
+        },
+        (edata) => {
+            def_log("Signup failed. Please try again... (" + edata + ")");
         }
-    });
-});
+    );
+}
+
+var login = (_usern, _password) => {
+    // Populate user data
+    app.setUserName(_usern);
+    
+    let userd = {
+        "username": app.user.username,
+        "password": _password,
+    };
+    
+    ajaxp(
+        app.urls.auth + "login", JSON.stringify(userd), def_headers,
+        (sdata) => {
+            def_log("Received data: " + sdata, false);
+              
+            let user = JSON.parse(sdata);
+            app.setUserInfo({
+                hasura_id: user["hasura_id"],
+                hasura_roles: user["hasura_roles"],
+                auth_token: user["auth_token"]
+            });
+            
+            def_log("Login complete. Redirecting to app...", false);
+            window.location.href = "/app";
+        },
+        (edata) => {
+            def_log("Login failed. Please try again... (" + edata + ")");
+        }
+    );
+}
+
+var logout = () => {
+    if (!app.user.token) return;
+    
+    ajaxp(
+        app.urls.auth + "user/logout", "", def_headers,
+        (sresp) => {
+            def_log("User logged out", false);
+            window.location.href = "/";
+        }
+    );
+    app.clearSession();
+}
