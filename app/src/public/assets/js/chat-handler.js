76e4923598324584e5a224ffdb57f4e7b99809f4
imvpn22 <imvpn22@gmail.com>
2017-08-15 18:54:56 +0530

    Improved UI wb+chat

--- app/src/public/assets/js/chat-handler.js
+++ app/src/public/assets/js/chat-handler.js
@@ -9,3 +9,8 @@ $('#send-msg').on('click', function(){
 	$('#chats').append(' <div class="msg you-sent"><span class="msg-owner">You : </span>				<span class="msg-text">' + $("#new-msg").val() + ' </span></div>');
 	$('#new-msg').val("");
 });
+
+$('#open_groups_nav').on('click', function(){
+	$('#groups_nav').toggleClass('hidden');
+	$('#groups_panel_back').toggleClass('hidden');
+});
