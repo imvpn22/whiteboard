adb5e95cb6f9e7594cd10c43301cb603d72b7b7a
Methusael Murmu <blendit07@gmail.com>
2017-08-19 04:53:12 +0530

    Integrated Socket.io

--- app/src/server.js
+++ app/src/server.js
@@ -1,21 +1,25 @@
+
 var express = require('express');
-var request = require('request');
 var wbController = require('./controllers/wbController');
 
+// Use 8080 for local development because you might already have apache running on 80
+const port = 8080;
+
 var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
 var path = require('path');
 
-// set up template engine
+// Set up template engine
 app.set('view engine', 'ejs');
 
-// static files
-app.use(express.static('public'));
+// Static files
+app.use(express.static(__dirname + '/public'));
 
-// fire controllers
-wbController(app);
+// Fire controllers
+wbController(app, io);
 
-
-var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`White Board app listening on port ${port}!`);
+server.listen(port, () => {
+  console.log(`Whiteboard listening on port ${port}!`);
 });
