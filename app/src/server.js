57668abbd22b68025dd9c128fc60f6d43d27f6cb
Methusael Murmu <blendit07@gmail.com>
2017-08-20 20:30:34 +0530

    Changed socket transports to polling

--- app/src/server.js
+++ app/src/server.js
@@ -3,11 +3,14 @@ var express = require('express');
 var wbController = require('./controllers/wbController');
 
 // Use 8080 for local development because you might already have apache running on 80
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 var app = express();
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
+
+var io = require('socket.io')(server, {
+    'transports': ['polling']
+});
 
 var path = require('path');
 
